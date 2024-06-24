12d703426ee1892bcedfcd60f70452ddb3a37a34
Aman Raghav <amanraghavkumar@gmail.com>
2024-06-24 12:49:01 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+hyper_e_dot='-e .'
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]  
+
+        if hyper_e_dot in requirements:
+            requirements.remove(hyper_e_dot)
+            
+        return requirements
+
+setup(
+    name='Diamond Price Prediction',
+    version='0.0.1',
+    author='Aman Raghav',
+    author_email='amanraghav19722@gmail.com',
+    install_requires=get_requirements('requirements.txt'),
+    packages=find_packages(),
+)
